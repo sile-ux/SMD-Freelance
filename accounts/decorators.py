14fe7cc05cd19f663a6831f099c3d26637b9957e
sile-ux <borisyaketcha19ryan@gmail.com>
2026-05-21 12:31:25 +0100

    Fix: Correction finale du type de choix par défaut pour le rôle utilisateur

---
+++ accounts/decorators.py
@@ -0,0 +1,21 @@
+# accounts/decorators.py
+from django.shortcuts import redirect
+from django.contrib import messages
+
+def freelance_required(view_func):
+
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.role == 'freelance':
+            return view_func(request, *args, **kwargs)
+        messages.error(request, "Accès refusé. Cet espace est réservé aux Freelancers.")
+        return redirect('dashboard')
+    return _wrapped_view
+
+def client_required(view_func):
+
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated and request.user.role == 'client':
+            return view_func(request, *args, **kwargs)
+        messages.error(request, "Accès refusé. Cet espace est réservé aux Clients.")
+        return redirect('dashboard')
+    return _wrapped_view
